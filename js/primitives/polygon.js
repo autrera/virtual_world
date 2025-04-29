fc7bd8d706276a4bdb316f9c0464a82381bbc6a0
Aldo Utrera <accounts@aldoutrera.com>
2025-04-29 18:40:05 -0500

    Break and draw the envelopes segments based on the intersections

--- js/primitives/polygon.js
+++ js/primitives/polygon.js
@@ -23,10 +23,22 @@ class Polygon {
         if (int && int.offset != 1 && int.offset != 0) {
           const point = new Point(int.x, int.y);
           intersections.push(point);
+          let aux = segs1[i].p1;
+          segs1[i].p2 = point;
+          segs1.splice(i + 1, 0, new Segment(point, aux));
+          aux = segs2[j].p1;
+          segs2[j].p2 = point;
+          segs2.splice(j + 1, 0, new Segment(point, aux));
         }
       }
     }
     return intersections;
+  }
+
+  drawSegments(ctx) {
+    for (const seg of this.segments) {
+      seg.draw(ctx, { color: getRandomColor(), width: 5 });
+    }
   }
 
   draw(
